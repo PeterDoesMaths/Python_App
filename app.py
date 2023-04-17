b701ef58e42e895a915f052617a1cdfc16ef3345
Peter Moskvichev <peter.mosk123@gmail.com>
2023-04-17 17:55:08 +0930

    adding web app

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import openai
 import os, json
-from flask import Flask
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template('upload.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the uploaded file
+    file = request.files['file']
+
+    # Read CSV file using Pandas
+    data = pd.read_csv(file)
+
+    # Display the first five rows of the data
+    return data.head().to_html()
+
+if __name__ == '__main__':
+    app.run(debug=True)
